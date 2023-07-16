eadec63e47765ad74b07a371fe10574dbbb6f560
baejunil <bjo6300@naver.com>
2023-07-16 16:18:41 +0900

    chore : 프로젝트 초기 세팅

---
+++ blido/src/main/java/org/programmers/blido/BlidoApplication.java
@@ -0,0 +1,13 @@
+package org.programmers.blido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlidoApplication.class, args);
+	}
+
+}
---
+++ blido/src/test/java/org/programmers/blido/BlidoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.programmers.blido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
