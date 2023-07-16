5c566fadb3c869b5ecfde2e730711cd3541f1fdc
baejunil <bjo6300@naver.com>
2023-07-17 00:45:40 +0900

    feat : PresentationCreateRequest 검증 추가

--- blido/src/main/java/org/programmers/blido/domain/presentation/dto/request/PresentationCreateRequest.java
+++ blido/src/main/java/org/programmers/blido/domain/presentation/dto/request/PresentationCreateRequest.java
@@ -1,11 +1,15 @@
 package org.programmers.blido.domain.presentation.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import lombok.Builder;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Builder
-public record PresentationCreateRequest(@JsonProperty String title, LocalDateTime startAt,
-                                        LocalDateTime endAt) {
+public record PresentationCreateRequest(@NotBlank String title, @NotNull
+@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime startAt,
+                                        @NotNull
+                                        @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime endAt) {
 
 }
