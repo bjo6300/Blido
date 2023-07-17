0b512b4f37aaac31f50a903ca69d10123e3ad8c6
baejunil <bjo6300@naver.com>
2023-07-18 03:31:26 +0900

    feat : springboot, mysql docker setting

--- blido/src/test/java/org/programmers/blido/BlidoApplicationTests.java
+++ blido/src/test/java/org/programmers/blido/BlidoApplicationTests.java
@@ -3,7 +3,7 @@ package org.programmers.blido;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BlidoApplicationTests {
 
 	@Test
