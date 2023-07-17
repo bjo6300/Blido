528be41d4e8f22bb4adb9b0dc2e3326867bcc337
baejunil <bjo6300@naver.com>
2023-07-17 18:21:37 +0900

    feat : BaseTimeEntity 구현

--- blido/src/main/java/org/programmers/blido/BlidoApplication.java
+++ blido/src/main/java/org/programmers/blido/BlidoApplication.java
@@ -2,12 +2,14 @@ package org.programmers.blido;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BlidoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlidoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BlidoApplication.class, args);
+  }
 
 }
